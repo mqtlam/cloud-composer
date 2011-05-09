bc9bbec106f017edaff9aa809cf2a7e0881d0a72
June <June@localhost>
2011-05-08 18:24:46 -0700

    other methods added

--- src/main/java/CloudComposerGroup/NoteGrid.java
+++ src/main/java/CloudComposerGroup/NoteGrid.java
@@ -7,14 +7,17 @@ import java.util.Map;
  * 
  */
 public class NoteGrid {
-	/** the grid that contains instrument notes **/
+	/** note grid that contains instrument notes **/
 	private List<HashMap<Integer, ArrayList<Note>>> grid;
+	/** the number of instrument notes on note grid **/
+	private int size;
 	
 	/**
 	 * Constructs a NoteGrid object
 	 */
 	public NoteGrid() {
 		grid = new ArrayList<HashMap<Integer, ArrayList<Note>>>();
+		size = 0;
 	}
 	
 	/**
@@ -25,9 +28,11 @@ public class NoteGrid {
 	 * @param instrument, integer that represents an instrument
 	 * @param pitch, integer that represents the pitch of a note
 	 * @requires column, length, instrument and pitch >= 0
-	 * @effects adds a note onto the NoteGrid
+	 * @modifies this
+	 * @effects adds a note onto the NoteGrid. If the same node already exists,
+	 * 			node will not be added at all.
 	 */
-	public void add(int column, Note note) {	
+	public void add(int column, Note note) {
 		Map<Integer, ArrayList<Note>> columns = grid.get(column);
 		if (columns == null) {
 			columns = new HashMap<Integer, ArrayList<Note>>();
@@ -37,31 +42,42 @@ public class NoteGrid {
 				grid.add(column, (HashMap<Integer, ArrayList<Note>>) columns);
 			}
 		} else {
-			boolean existed = true;
 			List<Note> notes = columns.get(note.pitch);
 			if (notes == null) {
 				notes = new ArrayList<Note>();
-				existed = false;
-			}
-			if (notes.add(note))
-				if (!existed)
+				if (notes.add(note))
 					columns.put(note.pitch, (ArrayList<Note>) notes);
+			} else
+				if (!notes.contains(note))
+					if (notes.add(note)) {}
 		}
+		size++;
 	}
 	
-	public void remove(int column, int length, int instrument, int pitch) {
-		 
+	public void remove(int column, Note note) {
+		Map<Integer, ArrayList<Note>> columns = grid.get(column);
+		List<Note> notes = columns.get(note.pitch);
+		notes.remove(note);
+		size--;
+	}
+
+	public List<Note> get(int column, int pitch) {
+		return grid.get(column).get(pitch);
 	}
 	
-	
-//	public boolean columnExists(int column) {
-//		return grid.get(column) != null;
-//	}
-//	
-//	public boolean noteExists(int column, Note note) {
-//		if (columnExists(column))
-//			for (Note n : grid.get(column).get(note.pitch))
-//				return (n.instrument == note.instrument && n.length == note.length);
-//		return false;
-//	}	
+	public boolean contains(int column, Note note) {
+		if (grid.get(column) != null)
+			for (Note n : grid.get(column).get(note.pitch))
+				return (n.instrument == note.instrument && n.length == note.length);
+		return false;
+	}	
+
+	public int size() {
+		return size;
+	}
+		
+	public void clear() {
+		grid.clear();
+	}
+
 }
