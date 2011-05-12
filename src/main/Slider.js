a9f577c9fe845021ab524ca91011ad4832c0d0dc
Hannah Hemmaplardh <hannah.hemmaplardh@gmail.com>
2011-05-11 22:00:41 -0700

    trying to get tempo bar and playback bar to use slider

--- src/main/Slider.js
+++ src/main/Slider.js
@@ -1,10 +1,10 @@
-function Slider(id, w, h) {
+function Slider(id, w, h, sliderID) {
 	var holder = document.createElement("div");
 	holder.style.width = w + "px";
 	holder.style.height = h + "px";
 	holder.style.position = "absolute";
 	holder.style.bottom = "2px";
-	holder.className = "slider";
+	holder.id = sliderID;
 
 	document.getElementById(id).appendChild(holder);
 	
