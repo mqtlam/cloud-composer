775cdc9108bb14cf16131f6933e7aa412d766126
faeornran <faeornran@gmail.com>
2011-05-13 16:52:58 -0700

    Forgot to save some files.

--- src/main/java/CloudComposerGroup/CloudComposer/SongSequence.java
+++ src/main/java/CloudComposerGroup/CloudComposer/SongSequence.java
@@ -13,10 +13,10 @@ import javax.sound.midi.Sequence;
  * saving them to sequence, and generating it.
  */
 public class SongSequence {
-	private int instruments;	// the number of instruments
+	//private int instruments;	// the number of instruments
 	private NoteGrid grid;		// note grid that contains all the notes
-	private Sequence sequence;  // sequence of song to be generated
-	private int ticksPerFrame;  // duration of sequence
+	//private Sequence sequence;  // sequence of song to be generated
+	//private int ticksPerFrame;  // duration of sequence
 		
 	/**
 	 * Constructs a SongSequence object
@@ -27,10 +27,10 @@ public class SongSequence {
 	 * @requires grid != null && ticksPerFrame > 0 && instruments > 0
 	 * @throws InvalidMidiDataException if it cannot create a sequence object
 	 */
-	public SongSequence(NoteGrid grid, int ticksPerFrame, int instruments) throws InvalidMidiDataException {
+	public SongSequence(NoteGrid grid) { //, int ticksPerFrame, int instruments) throws InvalidMidiDataException {
 		this.grid = grid;
-		this.ticksPerFrame = ticksPerFrame;
-		this.instruments = instruments;
+		//this.ticksPerFrame = ticksPerFrame;
+		//this.instruments = instruments;
 	}
 	
 	/**
