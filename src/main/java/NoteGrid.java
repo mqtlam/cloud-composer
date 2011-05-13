13b70c3a2e526375985824a4d9aee0d361560bd0
June <June@localhost>
2011-05-12 19:59:21 -0700

    NoteGrid class modified

--- src/main/java/NoteGrid.java
+++ src/main/java/NoteGrid.java
@@ -1,3 +1,5 @@
+package CloudComposerGroup;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -10,7 +12,6 @@ import java.util.Map;
  */
 public class NoteGrid {
 	/** note grid that contains instrument notes **/
-	//private Map<Integer, HashMap<Integer, ArrayList<Note>>> grid;
 	private Map<Integer, ArrayList<Note>> grid;
 	/** the number of instrument notes on note grid **/
 	private int size;
@@ -19,10 +20,9 @@ public class NoteGrid {
 	 * Constructs a NoteGrid object
 	 */
 	public NoteGrid() {
-		//grid = new HashMap<Integer, HashMap<Integer, ArrayList<Note>>>();
 		grid = new HashMap<Integer, ArrayList<Note>>();
 		size = 0;
-	}
+	}	
 	
 	/**
 	 * Adds a note in a proper position of note grid
@@ -33,29 +33,16 @@ public class NoteGrid {
 	 * @modifies this
 	 */
 	public void add(Note note, int column) {
-		//Map<Integer, ArrayList<Note>> columns = grid.get(column);
-		ArrayList<Note> columnNotes = grid.get(column);
-		//if (column == null) {
+		List<Note> columnNotes = grid.get(column);
 		if (columnNotes == null) {
-			//HashMap<Integer, ArrayList<Note>> newColumn = new HashMap<Integer, ArrayList<Note>>();
-			ArrayList<Note> newColumn = new ArrayList<Note>();
-			//ArrayList<Note> newRow = new ArrayList<Note>();
-			//newRow.add(note);
-			//newColumn.put(note.pitch, newRow);
-			
-			grid.put(column, newColumn);
+			columnNotes = new ArrayList<Note>();
+			columnNotes.add(note);
+			grid.put(column, (ArrayList<Note>) columnNotes);
 		} else {
-			ArrayList<Note> row = columns.get(columns);
-			if (row == null) {
-				ArrayList<Note> newRow = new ArrayList<Note>();
-				newRow.add(note);
-				columns.put(note.pitch, newRow);
-			} else
-				if (!row.contains(note)) {
-					row.add(note);
-					columns.put(note.pitch, row);
-				} else
-					return;
+			if (!columnNotes.contains(note))
+				columnNotes.add(note);
+			else
+				return;				
 		}
 		size++;
 	}
@@ -69,36 +56,34 @@ public class NoteGrid {
 	 * @modifies this
 	 */
 	public void remove(Note note, int column) {
-		Map<Integer, ArrayList<Note>> columns = grid.get(column);
-		ArrayList<Note> row = columns.get(note.pitch);
-		row.remove(note);
+		List<Note> columnNotes = grid.get(column);
+		columnNotes.remove(note);
 		size--;
 	}
 	
 	/**
-	 * true if it removes all the notes of a particular column and pitch.
+	 * Returns true if it removes all the notes of a particular column.
 	 * Otherwise, false.
 	 * 
 	 * @param column, integer that represents a column of the note grid
-	 * @param pitch, integer that represents a pitch of a note
-	 * @requires column && pitch >= 0
+	 * @requires column >= 0
 	 * @modifies this
 	 */
-	public boolean removeAllNotes(int column, int pitch) {
-		size -= grid.get(column).get(pitch).size();
-		return grid.get(column).remove(pitch) != null;
+	public boolean removeAllNotes(int column) {
+		size -= grid.get(column).size();
+		return grid.remove(column) != null;
 	}
 	
 	/**
-	 * Returns a copy of a list of notes of given column and pitch
+	 * Returns a copy of a list of notes of given column
 	 * 
 	 * @param column, index of columns
-	 * @param pitch, integer that represents pitch of note
-	 * @requires column & pitch >= 0
-	 * @return a list of notes of given column and pitch
+	 * @requires column >= 0
+	 * @return list of notes of column
 	 */
-	public List<Note> getNotes(int column, int pitch) {
-		return new ArrayList<Note>(grid.get(column).get(pitch));
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public List<Note> getNotes(int column) {
+		return new ArrayList(grid.get(column));
 	}
 	
 	/**
@@ -110,18 +95,7 @@ public class NoteGrid {
 		Collections.sort(columns);
 		return columns;
 	}
-	
-	/**
-	 * Returns HashMap object to provide the notes of a different pitch of a column
-	 * 
-	 * @param column, integer that represents a column of note grid
-	 * @requires column >= 0
-	 * @return HashMap object that contains the notes of a different pitch corresponding to a particular column
-	 */
-	public HashMap<Integer, ArrayList<Note>> getPitches(int column) {
-		return grid.get(column);
-	}
-	
+		
 	/**
 	 * Returns true if note already exists.
 	 * Otherwise, returns false.
@@ -132,8 +106,8 @@ public class NoteGrid {
 	 */
 	public boolean contains(Note note, int column) {
 		if (grid.get(column) != null)
-			for (Note n : grid.get(column).get(note.pitch))
-				return (n.instrument == note.instrument && n.length == note.length);
+			for (Note n : grid.get(column))
+				return n.equals(note);
 		return false;
 	}
 	
