12c76931cdcaba074e2c1176521543ede5af0b4e
June <June@localhost>
2011-05-15 17:38:34 -0700

    test cases for every method of NoteGrid class created

--- src/test/java/CloudComposerGroup/CloudComposer/NoteGridTest.java
+++ src/test/java/CloudComposerGroup/CloudComposer/NoteGridTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.matchers.JUnitMatchers.*;
 import CloudComposerGroup.CloudComposer.*;
-
+import java.util.*;
 
 public class NoteGridTest  {
 	
@@ -24,8 +24,6 @@ public class NoteGridTest  {
 	private Note n7;
 	private Note n8;
 	private Note n9;
-	
-	private Note n10;
 
 	/**
 	 * sets NoteGrid and Note objects every single time it runs a test
@@ -48,6 +46,7 @@ public class NoteGridTest  {
 	
 	/**
 	 * Test if note grid adds note objects in a column correctly or not
+	 * Test the add & contains functions
 	 */
 	@Test
 	public void testNoteGridAdd00() {
@@ -58,6 +57,7 @@ public class NoteGridTest  {
 	
 	/**
 	 * Test if note grid adds note objects in a column correctly or not
+	 * Test the add & contains functions
 	 */
 	@Test
 	public void testNoteGridAdd01() {
@@ -70,6 +70,7 @@ public class NoteGridTest  {
 	
 	/**
 	 * Test if note grid adds note objects in a column correctly or not
+	 * Test the add & contains functions
 	 */
 	@Test
 	public void testNoteGridAdd02() {
@@ -80,6 +81,7 @@ public class NoteGridTest  {
 	
 	/**
 	 * Test if note grid adds note objects in a column correctly or not
+	 * Test the add & contains functions
 	 */
 	@Test
 	public void testNoteGridAdd03() {
@@ -90,6 +92,7 @@ public class NoteGridTest  {
 	
 	/**
 	 * Test if note grid adds note objects in a column correctly or not
+	 * Test the add & contains functions
 	 */
 	@Test
 	public void testNoteGridAdd04() {
@@ -103,6 +106,7 @@ public class NoteGridTest  {
 
 	/**
 	 * Test if note grid adds note objects in a column correctly or not
+	 * Test the add & contains functions
 	 */
 	@Test
 	public void testNoteGridAdd05() {
@@ -121,6 +125,7 @@ public class NoteGridTest  {
 
 	/**
 	 * Test if note grid adds note objects in a column correctly or not
+	 * Test the add & contains functions
 	 */
 	@Test
 	public void testNoteGridAdd06() {
@@ -139,6 +144,7 @@ public class NoteGridTest  {
 		
 	/**
 	 * Test if note grid removes note objects in a column correctly or not
+	 * Test the remove & contains functions
 	 */
 	@Test
 	public void testNoteGridRemove01() {
@@ -149,6 +155,7 @@ public class NoteGridTest  {
 	
 	/**
 	 * Test if note grid removes note objects in a column correctly or not
+	 * Test the remove & contains functions
 	 */
 	@Test
 	public void testNoteGridRemove02() {
@@ -162,6 +169,7 @@ public class NoteGridTest  {
 
 	/**
 	 * Test if note grid removes note objects in a column correctly or not
+	 * Test the remove & contains functions
 	 */
 	@Test
 	public void testNoteGridRemove03() {
@@ -185,6 +193,7 @@ public class NoteGridTest  {
 	
 	/**
 	 * Test if note grid removes note objects in a column correctly or not
+	 * Test the remove & contains functions
 	 */
 	@Test
 	public void testNoteGridRemove05() {
@@ -198,6 +207,7 @@ public class NoteGridTest  {
 	
 	/**
 	 * Test if note grid removes note objects in a column correctly or not
+	 * Test the remove & contains functions
 	 */
 	@Test
 	public void testNoteGridRemove06() {
@@ -209,22 +219,471 @@ public class NoteGridTest  {
 		assertFalse(grid.contains(n0, 9));
 		assertFalse(grid.contains(n1, 8));
 	}
-
 	
-	public void testNoteGridContains() {
+	/**
+	 * Test if note grid returns a list of the notes of a column correctly or not
+	 */
+	@Test
+	public void testNoteGridGetNotes00() {
+		List<Note> actual = grid.getNotes(0);
 
-		NoteGrid grid = new NoteGrid();
-		Note note = new Note(1, 2, 3);
-
-		// Test the add & contains functions
-		grid.add(note, 3);
-		grid.add(note, 5);
-		grid.add(note, 7);
-		grid.add(note, 100);
-		assertTrue(grid.contains(note, 3));
-		assertTrue(grid.contains(note, 5));
-		assertTrue(grid.contains(note, 7));
-		assertTrue(grid.contains(note, 100));
+		assertNull(actual);
+	}
+	
+	/**
+	 * Test if note grid returns a list of the notes of a column correctly or not
+	 */
+	@Test
+	public void testNoteGridGetNotes01() {
+		grid.add(n0, 0);
+		List<Note> actual = grid.getNotes(0);
+		List<Note> expected = new ArrayList<Note>();
+		expected.add(n0);
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if note grid returns a list of the notes of a column correctly or not
+	 */
+	@Test
+	public void testNoteGridGetNotes02() {
+		grid.add(n0, 0);
+		grid.add(n1, 0);
+		List<Note> actual = grid.getNotes(0);
+		List<Note> expected = new ArrayList<Note>();
+		expected.add(n0);
+		expected.add(n1);
+		
+		assertEquals(expected, actual);
 	}
 
+	/**
+	 * Test if note grid returns a list of the notes of a column correctly or not
+	 */
+	@Test
+	public void testNoteGridGetNotes03() {
+		grid.add(n0, 1);
+		grid.add(n1, 1);
+		List<Note> actual = grid.getNotes(1);
+		List<Note> expected = new ArrayList<Note>();
+		expected.add(n0);
+		expected.add(n1);
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns a list of the notes of a column correctly or not
+	 */
+	@Test
+	public void testNoteGridGetNotes04() {
+		grid.add(n0, 2);
+		grid.add(n1, 2);
+		List<Note> actual = grid.getNotes(2);
+		List<Note> expected = new ArrayList<Note>();
+		expected.add(n0);
+		expected.add(n1);
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns a list of the notes of a column correctly or not
+	 */
+	@Test
+	public void testNoteGridGetNotes05() {
+		grid.add(n0, 8);
+		grid.add(n1, 8);
+		List<Note> actual = grid.getNotes(8);
+		List<Note> expected = new ArrayList<Note>();
+		expected.add(n0);
+		expected.add(n1);
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns a list of the notes of a column correctly or not
+	 */
+	@Test
+	public void testNoteGridGetNotes06() {
+		grid.add(n0, 9);
+		grid.add(n1, 9);
+		List<Note> actual = grid.getNotes(9);
+		List<Note> expected = new ArrayList<Note>();
+		expected.add(n0);
+		expected.add(n1);
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns a list of the notes of a column correctly or not
+	 */
+	@Test
+	public void testNoteGridGetNotes07() {
+		grid.add(n0, 0);
+		grid.add(n1, 0);
+		grid.add(n2, 0);
+		grid.add(n3, 0);
+		grid.add(n4, 0);
+		List<Note> actual = grid.getNotes(0);
+		
+		List<Note> expected = new ArrayList<Note>();
+		expected.add(n0);
+		expected.add(n1);
+		expected.add(n2);
+		expected.add(n3);
+		expected.add(n4);
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns a list of the notes of a column correctly or not
+	 */
+	@Test
+	public void testNoteGridGetNotes08() {
+		grid.add(n5, 0);
+		grid.add(n6, 0);
+		grid.add(n7, 0);
+		grid.add(n8, 0);
+		grid.add(n9, 0);
+		List<Note> actual = grid.getNotes(0);
+		
+		List<Note> expected = new ArrayList<Note>();
+		expected.add(n5);
+		expected.add(n6);
+		expected.add(n7);
+		expected.add(n8);
+		expected.add(n9);
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if note grid returns a set of integers that represents each column of grid
+	 */
+	@Test
+	public void testNoteGridGetColumns01() {
+		grid.add(n0, 0);
+		
+		List<Integer> actual = new ArrayList<Integer>(grid.getColumns());
+		List<Integer> expected = new ArrayList<Integer>();
+		expected.add(0);
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if note grid returns a set of integers that represents each column of grid
+	 */
+	@Test
+	public void testNoteGridGetColumns02() {
+		grid.add(n0, 0);
+		grid.add(n0, 1);
+		
+		List<Integer> actual = new ArrayList<Integer>(grid.getColumns());
+		Collections.sort(actual);
+		List<Integer> expected = new ArrayList<Integer>();
+		expected.add(0);
+		expected.add(1);
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns a set of integers that represents each column of grid
+	 */
+	@Test
+	public void testNoteGridGetColumns03() {
+		grid.add(n0, 0);
+		grid.add(n0, 1);
+		grid.add(n0, 2);
+		
+		List<Integer> actual = new ArrayList<Integer>(grid.getColumns());
+		Collections.sort(actual);
+		List<Integer> expected = new ArrayList<Integer>();
+		expected.add(0);
+		expected.add(1);
+		expected.add(2);
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns a set of integers that represents each column of grid
+	 */
+	@Test
+	public void testNoteGridGetColumns04() {
+		grid.add(n0, 9);
+		
+		List<Integer> actual = new ArrayList<Integer>(grid.getColumns());
+		Collections.sort(actual);
+		List<Integer> expected = new ArrayList<Integer>();
+		expected.add(9);
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns a set of integers that represents each column of grid
+	 */
+	@Test
+	public void testNoteGridGetColumns05() {
+		grid.add(n0, 0);
+		grid.add(n0, 1);
+		grid.add(n0, 2);
+		grid.add(n0, 3);
+		grid.add(n0, 4);
+		
+		List<Integer> actual = new ArrayList<Integer>(grid.getColumns());
+		Collections.sort(actual);
+		List<Integer> expected = new ArrayList<Integer>();
+		expected.add(0);
+		expected.add(1);
+		expected.add(2);
+		expected.add(3);
+		expected.add(4);
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns a set of integers that represents each column of grid
+	 */
+	@Test
+	public void testNoteGridGetColumns06() {
+		grid.add(n0, 5);
+		grid.add(n0, 6);
+		grid.add(n0, 7);
+		grid.add(n0, 8);
+		grid.add(n0, 9);
+		
+		List<Integer> actual = new ArrayList<Integer>(grid.getColumns());
+		Collections.sort(actual);
+		List<Integer> expected = new ArrayList<Integer>();
+		expected.add(5);
+		expected.add(6);
+		expected.add(7);
+		expected.add(8);
+		expected.add(9);
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if note grid returns a set of integers that represents each column of grid
+	 */
+	@Test
+	public void testNoteGridGetColumns07() {
+		grid.add(n0, 0);
+		grid.add(n0, 1);
+		grid.add(n0, 2);
+		grid.add(n0, 3);
+		grid.add(n0, 4);
+		grid.add(n0, 5);
+		grid.add(n0, 6);
+		grid.add(n0, 7);
+		grid.add(n0, 8);
+		grid.add(n0, 9);
+		
+		List<Integer> actual = new ArrayList<Integer>(grid.getColumns());
+		Collections.sort(actual);
+		List<Integer> expected = new ArrayList<Integer>();
+		expected.add(0);
+		expected.add(1);
+		expected.add(2);
+		expected.add(3);
+		expected.add(4);
+		expected.add(5);
+		expected.add(6);
+		expected.add(7);
+		expected.add(8);
+		expected.add(9);
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if note grid returns its size correctly or not
+	 */
+	@Test
+	public void testNoteGridSize00() {
+		int actual = grid.size();
+		int expected = 0;
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns its size correctly or not
+	 */
+	@Test
+	public void testNoteGridSize01() {
+		grid.add(n0, 0);
+		
+		int actual = grid.size();
+		int expected = 1;
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if note grid returns its size correctly or not
+	 */
+	@Test
+	public void testNoteGridSize02() {
+		grid.add(n0, 0);
+		grid.add(n0, 1);
+		
+		int actual = grid.size();
+		int expected = 2;
+		
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if note grid returns its size correctly or not
+	 */
+	@Test
+	public void testNoteGridSize03() {
+		grid.add(n0, 9);
+		
+		int actual = grid.size();
+		int expected = 1;
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if note grid returns its size correctly or not
+	 */
+	@Test
+	public void testNoteGridSize04() {
+		grid.add(n0, 8);
+		grid.add(n0, 9);
+		
+		int actual = grid.size();
+		int expected = 2;
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if note grid returns its size correctly or not
+	 */
+	@Test
+	public void testNoteGridSize05() {
+		grid.add(n0, 4);
+		grid.add(n1, 5);
+		grid.add(n2, 6);
+		grid.add(n3, 7);
+		grid.add(n4, 8);
+		grid.add(n5, 9);
+
+		int actual = grid.size();
+		int expected = 6;
+		
+		assertEquals(expected, actual);
+	}	
+	
+	/**
+	 * Test if note grid clears itself correctly or not
+	 */
+	@Test
+	public void testNoteGridClear01() {
+		grid.add(n0, 1);		
+		grid.clear();
+		assertFalse(grid.contains(n0, 1));
+	}
+	
+	/**
+	 * Test if note grid clears itself correctly or not
+	 */
+	@Test
+	public void testNoteGridClear02() {
+		grid.add(n0, 1);
+		grid.add(n1, 1);
+		grid.clear();
+		assertFalse(grid.contains(n0, 1));
+		assertFalse(grid.contains(n1, 1));
+	}
+
+	/**
+	 * Test if note grid clears itself correctly or not
+	 */
+	@Test
+	public void testNoteGridClear03() {
+		grid.add(n0, 2);
+		grid.add(n1, 2);
+		grid.clear();
+		assertFalse(grid.contains(n0, 2));
+		assertFalse(grid.contains(n1, 2));
+	}
+
+	/**
+	 * Test if note grid clears itself correctly or not
+	 */
+	@Test
+	public void testNoteGridClear04() {
+		grid.add(n0, 9);		
+		grid.clear();
+		assertFalse(grid.contains(n0, 9));
+	}
+	
+	/**
+	 * Test if note grid clears itself correctly or not
+	 */
+	@Test
+	public void testNoteGridClear05() {
+		grid.add(n0, 9);
+		grid.add(n1, 9);		
+
+		grid.clear();
+		assertFalse(grid.contains(n0, 9));
+	}
+	
+	/**
+	 * Test if note grid clears itself correctly or not
+	 */
+	@Test
+	public void testNoteGridClear06() {
+		grid.add(n0, 9);
+		grid.add(n1, 8);		
+
+		grid.clear();
+		assertFalse(grid.contains(n0, 9));
+		assertFalse(grid.contains(n1, 8));
+	}
+	
+	/**
+	 * Test if note grid clears itself correctly or not
+	 */
+	@Test
+	public void testNoteGridClear07() {
+		grid.add(n0, 0);
+		grid.add(n1, 1);		
+		grid.add(n2, 2);
+		grid.add(n3, 3);		
+		grid.add(n4, 4);
+		grid.add(n5, 5);		
+		grid.add(n6, 6);
+		grid.add(n7, 7);		
+		grid.add(n8, 8);
+		grid.add(n9, 9);		
+
+		grid.clear();
+		assertFalse(grid.contains(n0, 0));
+		assertFalse(grid.contains(n1, 1));
+		assertFalse(grid.contains(n2, 2));
+		assertFalse(grid.contains(n3, 3));
+		assertFalse(grid.contains(n4, 4));
+		assertFalse(grid.contains(n5, 5));
+		assertFalse(grid.contains(n6, 6));
+		assertFalse(grid.contains(n7, 7));
+		assertFalse(grid.contains(n8, 8));
+		assertFalse(grid.contains(n9, 9));
+	}
 }
