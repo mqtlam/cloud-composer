e9553463985662abc5c110842efa9721e370768d
faeornran <faeornran@gmail.com>
2011-05-19 13:15:42 -0700

    Added a way to push tests quickly with moveToTest.bash. Works the same as moveToRelease.bash, except to the test directory.

---
+++ moveToTest.bash
@@ -0,0 +1,79 @@
+#!/bin/bash
+if [ $# -lt 2 ]; then
+	echo "Usage: ./moveToRelease.bash <version number> <ftp password>"
+	exit 1
+fi
+release="beta"
+versionNum=$1
+workingDir="src/main/java/CloudComposerGroup/CloudComposer"
+toDir="release/${release}_${versionNum}"
+`mkdir -p $toDir`
+`mkdir "${toDir}/include"`
+`mkdir "${toDir}/images"`
+
+`javac "${workingDir}/"*.java`
+`jar -cvf $workingDir/MidiPlayer.jar $workingDir/*.class | echo`
+`rm "${workingDir}/"*.class`
+`mv "${workingDir}/MidiPlayer.jar" $toDir`
+
+workingDir="src/main/webapp"
+`cp "${workingDir}/"*.html "${toDir}/"`
+`cp "${workingDir}/"*.php "${toDir}/"`
+`cp "${workingDir}/"*.js "${toDir}/include/"`
+`cp "${workingDir}/"*.css "${toDir}/include/"`
+
+workingDir="src/main/resources"
+`cp "${workingDir}/"* "${toDir}/images/"`
+
+username="cloudcomposer@publicstaticdroid.com"
+server="ftp.publicstaticdroid.com"
+port="21"
+password=$2
+#`scp -p $port "${toDir}/*" "${username}:cloudcomposer/"`
+
+#files=$toDir/*
+
+ftp -n $server <<END_SCRIPT
+quote USER $username
+quote PASS $password
+mkdir test
+cd test
+mkdir include
+mkdir images
+mkdir lib
+mkdir resources
+mkdir songs
+END_SCRIPT
+
+for ff in $toDir/*; do
+file="test/${ff#*/*/}"
+ftp -n $server <<END_SCRIPT
+quote USER $username
+quote PASS $password
+binary
+put $ff $file
+quit
+END_SCRIPT
+done
+
+for ff in $toDir/include/*; do
+file="test/${ff#*/*/}"
+ftp -n $server <<END_SCRIPT
+quote USER $username
+quote PASS $password
+binary
+put $ff $file
+quit
+END_SCRIPT
+done
+
+for ff in $toDir/images/*; do
+file="test/${ff#*/*/}"
+ftp -n $server <<END_SCRIPT
+quote USER $username
+quote PASS $password
+binary
+put $ff $file
+quit
+END_SCRIPT
+done
