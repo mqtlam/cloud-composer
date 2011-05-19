57674650f2bccb697b767d376db6f99931c7030a
June <June@localhost>
2011-05-19 01:41:24 -0700

    more test cases added

--- src/test/java/CloudComposerGroup/CloudComposer/CloudMidiPlayerTest.java
+++ src/test/java/CloudComposerGroup/CloudComposer/CloudMidiPlayerTest.java
@@ -57,5 +57,71 @@ public class CloudMidiPlayerTest {
 		player.setTempo((float) 5.1521136);
 		assertEquals((float) 5.1521136, player.getTempo(), (float) 1.115);
 	}
+	
+	/**
+	 * Test if CloudMidiPlayer sets and returns a sequence correctly or not 
+	 * 
+	 * @throws InvalidMidiDataException
+	 */
+	@Test
+	public void testSetGetSequence01() throws InvalidMidiDataException {
+		Sequence expected = new Sequence(0, 0);
+		player.setSequence(expected);
+		Sequence actual = player.getSequence();
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if CloudMidiPlayer sets and returns a sequence correctly or not 
+	 * 
+	 * @throws InvalidMidiDataException
+	 */
+	@Test
+	public void testSetGetSequence02() throws InvalidMidiDataException {
+		Sequence expected = new Sequence(Sequence.SMPTE_24, 0);
+		player.setSequence(expected);
+		Sequence actual = player.getSequence();
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if CloudMidiPlayer sets and returns a sequence correctly or not 
+	 * 
+	 * @throws InvalidMidiDataException
+	 */
+	@Test
+	public void testSetGetSequence03() throws InvalidMidiDataException {
+		Sequence expected = new Sequence(Sequence.PPQ, 0);
+		player.setSequence(expected);
+		Sequence actual = player.getSequence();
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test if CloudMidiPlayer sets and returns a sequence correctly or not 
+	 * 
+	 * @throws InvalidMidiDataException
+	 */
+	@Test
+	public void testSetGetSequence04() throws InvalidMidiDataException {
+		Sequence expected = new Sequence(Sequence.SMPTE_25, 0);
+		player.setSequence(expected);
+		Sequence actual = player.getSequence();
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test if CloudMidiPlayer sets and returns a sequence correctly or not 
+	 * 
+	 * @throws InvalidMidiDataException
+	 */
+	@Test
+	public void testSetGetSequence05() throws InvalidMidiDataException {
+		Sequence expected = new Sequence(Sequence.SMPTE_30DROP, 0);
+		player.setSequence(expected);
+		Sequence actual = player.getSequence();
+		assertEquals(expected, actual);
+	}
+
 
 }
