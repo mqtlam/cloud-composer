849a779bc3bd3533a86b6019fdd8c89f983313e3
June <June@localhost>
2011-05-20 10:36:12 -0700

    test cases for getInstrument() created

---
+++ src/main/java/CloudComposerGroup/CloudComposer/CloudAppletController.java
@@ -0,0 +1,139 @@
+package CloudComposerGroup.CloudComposer;
+
+
+import java.applet.Applet;
+import java.io.IOException;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.Sequence;
+
+//CloudAppletController.java
+//This file is the applet used to interact between the UI,
+//the Java model of the grid, the NoteGrid to MIDI Sequence converter
+//and the Midi Player itself.
+public class CloudAppletController extends Applet { //implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	public CloudMidiPlayer player;
+	private SongSequence sequencer;
+	public NoteGrid grid;
+	private boolean changed;
+	
+	// Initializes the Applet and its components.
+	public void init() {
+		try {
+			grid = new NoteGrid();
+			player = new CloudMidiPlayer();
+			sequencer = new SongSequence(grid);
+			changed = false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Adds the provided data as a note on the NoteGrid.
+	public void addNote(int[] noteData)
+	{
+		addRemoveHelper(noteData, true);
+	}
+	
+	// Removes the provided data as a note from the NoteGrid.
+	public void removeNote(int[] noteData)
+	{
+		addRemoveHelper(noteData, false);
+	}
+	
+	// noteData format: [instrument, pitch, startPos, stopPos]
+	// Adds or removes a note given the data for the note.
+	private void addRemoveHelper(int[] noteData, boolean add) {
+		changed = true;
+		int length = noteData[3] - noteData[2] + 1;    // for example, starting column is 2 and ending column is 5. 5 - 2 + 1 = 4: 2, 3, 4, 5 
+		
+		if (add)
+			grid.add(new Note(length, noteData[0], noteData[1]), noteData[2]);	
+		else
+			grid.remove(new Note(length, noteData[0], noteData[1]), noteData[2]);
+	}
+	
+	// Returns a list of instruments.
+	public String[] getInstruments() {
+		String[] list = new String[5];
+		for (int i = 0; i < 5; i++) {
+			list[i] = player.getInstruments()[i].name();
+		}
+		return list;
+	}
+	
+	// Checks if the song has changed since it last played.
+	// If so, it remakes the song and then plays.  Otherwise, it plays
+	// what it currently has loaded.
+	public void play() throws InvalidMidiDataException
+	{
+		updateSequence();
+		player.play();
+	}
+	
+	// Plays the note indicated by the instrument and the pitch.
+	public void playNote(int instrument, int pitch) throws InvalidMidiDataException 
+	{
+		player.playNote(player.getInstruments()[instrument], pitch);
+	}
+	
+	// Stops the song, moving its playback position to the beginning.
+	public void stop() 
+	{
+		player.stop();
+	}
+	
+	// Stops a song, but doesn't change the playback position.
+	public void pause()
+	{
+		player.pause();
+	}
+	
+	// Updates the sequence and then tells the Midiplayer to write the Midi file.
+	public void download(String location) throws InvalidMidiDataException, IOException 
+	{
+		updateSequence();
+		player.writeToFile(location);
+	}
+	
+	// Sets the current position of the song in the player.
+	public void setSongPosition(int column) 
+	{
+		player.setPlayTime(column);
+	}
+	
+	// Gets the current song position.
+	public int currentSongPosition()
+	{
+		return player.playbackBarColumn();
+	}
+	
+	// Sets the tempo to the provided BPM.
+	public void setTempo(float bpm) throws InvalidMidiDataException
+	{
+		player.setTempo(bpm);
+	}
+	
+	// Returns the current Midi song data, regardless of if it has been modified.
+	public Sequence getSongSequence() {
+		return player.getSequence();
+	}
+	
+	// Determines if a song is currently being played.
+	public boolean isPlaying() {
+		return player.isPlaying();
+	}
+	
+	// Updates the Midiplayer with the actual song data.
+	private void updateSequence() throws InvalidMidiDataException {
+		if (changed)
+		{
+			changed = false;
+			Sequence s = sequencer.getSequence();
+			player.setSequence(s);
+		}
+	}
+
+}
---
+++ src/main/webapp/Note.js
@@ -0,0 +1,21 @@
+//	Cloud Composer. Eui Jung. May 5, 2011
+//	Contains the definition for Note Object
+
+
+function Note(len, inst, pitch) {
+	this.noteLength = len;
+	this.instrument = inst;
+	this.pitch = pitch;
+}
+
+// pre-condition:
+// two notes can't have same instruments and pitch but different length
+Note.prototype.equals = function (note) {
+	return this.instrument.equals(note.instrument)
+		&& this.pitch == note.pitch;
+}
+	
+Note.prototype.changeLength = function (newLength) {
+	this.noteLength = newLength;
+}
+
