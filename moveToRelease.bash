3ab3e2f3d6f2a77348b64752096464734e4d9e8e
faeornran <faeornran@gmail.com>
2011-05-20 13:21:33 -0700

    moveToRelease.bash now is used for releasing both to tests and releases.

--- moveToRelease.bash
+++ moveToRelease.bash
@@ -1,20 +1,32 @@
 #!/bin/bash
-if [ $# -lt 2 ]; then
-	echo "Usage: ./moveToRelease.bash <version number> <ftp password>"
+if [[ ($# -lt 2) || (($# -gt 2) && ($3 != "--release")) ]]; then
+	echo "Usage: ./moveToRelease.bash <version number> <ftp password> [--release]"
 	exit 1
+fi
+
+releaseType="test"
+if [[ ($# -gt 2) ]]; then
+	releaseType="release"
 fi
 release="featurecomplete"
 versionNum=$1
-workingDir="src/main/java/CloudComposerGroup/CloudComposer"
-toDir="release/${release}_${versionNum}"
+currentDir=$PWD
+toDir="${releaseType}/${release}_${versionNum}"
+
 `mkdir -p $toDir`
 `mkdir "${toDir}/include"`
 `mkdir "${toDir}/images"`
 
+workingDir="src/main/java/CloudComposerGroup/CloudComposer"
+tempDir="CloudComposerGroup/CloudComposer"
 `javac "${workingDir}/"*.java`
-`jar -cvf $workingDir/MidiPlayer.jar $workingDir/*.class | echo`
-`rm "${workingDir}/"*.class`
-`mv "${workingDir}/MidiPlayer.jar" $toDir`
+`mkdir -p $tempDir`
+`mv $workingDir/*.class $tempDir/.`
+`jar -cvf MidiPlayer.jar $tempDir/*.class | echo`
+password=$2
+`jarsigner -storepass $password -keypass $password MidiPlayer.jar cloudcomposer | echo`
+`rm -r $tempDir`
+`mv MidiPlayer.jar $toDir`
 
 workingDir="src/main/webapp"
 `cp "${workingDir}/"*.html "${toDir}/"`
@@ -28,40 +40,25 @@ workingDir="src/main/resources"
 username="cloudcomposer@publicstaticdroid.com"
 server="ftp.publicstaticdroid.com"
 port="21"
-password=$2
-#`scp -p $port "${toDir}/*" "${username}:cloudcomposer/"`
 
-#files=$toDir/*
-
-for ff in $toDir/*; do
-file=${ff#*/*/}
-ftp -n $server <<END_SCRIPT
+ftp -ni $server <<END_SCRIPT
 quote USER $username
 quote PASS $password
 binary
-put $ff $file
-quit
-END_SCRIPT
-done
 
-for ff in $toDir/include/*; do
-file=${ff#*/*/}
-ftp -n $server <<END_SCRIPT
-quote USER $username
-quote PASS $password
-binary
-put $ff $file
-quit
-END_SCRIPT
-done
+mkdir test
+cd $releaseType
+mkdir include
+mkdir images
+mkdir lib
+mkdir resources
+mkdir songs
 
-for ff in $toDir/images/*; do
-file=${ff#*/*/}
-ftp -n $server <<END_SCRIPT
-quote USER $username
-quote PASS $password
-binary
-put $ff $file
-quit
+lcd $toDir
+cd $releaseType
+mput *
+mput images/*
+mput include/*
+bye
+
 END_SCRIPT
-done
