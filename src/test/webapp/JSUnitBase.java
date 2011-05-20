888144a02ae16b35547a79c9e57cded9e5d75e8f
Jungryul Choi <Jungryul Choi@localhost>
2011-05-19 22:36:45 -0700

    xxxTest.java files Removed. Without own java file, xxxTest.html files can be tested.
    testSuite.html Created
    One test suite contains all test cases(xxxTest.html).
    
    JSUnitBase Class Modified.

--- src/test/webapp/JSUnitBase.java
+++ src/test/webapp/JSUnitBase.java
@@ -1,17 +1,23 @@
 package com.publicstaticdroid.cloudcomposer;
 import net.jsunit.StandaloneTest;
 
+// JSUnitBase Class manipulates JsUnit Test Cases. This class is run by JUnit, which is also built by Maven(or Ant).
+// Read more details at JsUnit Server, http://jsunit.net/
 public abstract class JSUnitBase extends StandaloneTest {
 
 	public JSUnitBase(String string) {
 		super(string);
 		String basePath = "./";
 		String testRunner = basePath + "testRunner.html"; // relative path to test runner
-		String testPage = basePath + getTestPageRelative(); // relative path to actual test file
-		
-		String url = testRunner+"?testPage="+testPage+"&autoRun=true&submitresults=true"+"="+basePath; // do we want the results to appear in basePath (src/test.webapp)?
+		String testPage = basePath + "testSuite.html"; // relative path to actual test file
 
-		String firefox4 = "/usr/bin/firefox"; // TODO
+		// JsUnit automatically tests all test cases, and output the result to /jsunit/acceptor
+		// The result directory can be changed by submitResults=URL (e.g. "sumitResults=localhost:8080/jsunit/acceptor") 
+		// The result will be stored with ID(which is current time in millisecond) in XML format using Java Servlet.
+		String url = testRunner+"?testPage="+testPage+"&autoRun=true&submitResults=true";
+		/* "+"="+basePath; // do we want the results to appear in basePath (src/test.webapp)? */
+
+		String firefox4 = "/usr/bin/firefox"; // TODO: Set this the path of FireFox on the server
 
 		System.setProperty("browserFileNames", firefox4);
 		System.setProperty("url", url);
