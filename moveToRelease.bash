3ba06b2aa4ce89778b4acdb9cec252b048c8c97e
faeornran <faeornran@gmail.com>
2011-05-20 13:47:08 -0700

    Cleaned up moveToRelease.bash and commented.

--- moveToRelease.bash
+++ moveToRelease.bash
@@ -1,22 +1,38 @@
 #!/bin/bash
+
+# moveToRelease.bash
+# This script will push the latest files to the FTP server specified within.
+
+# Test if the program is being used with an appropriate detail set.
 if [[ ($# -lt 2) || (($# -gt 2) && ($3 != "--release")) ]]; then
 	echo "Usage: ./moveToRelease.bash <version number> <ftp password> [--release]"
 	exit 1
 fi
 
+# Modify these variables for your personal FTP server.
+username="cloudcomposer@publicstaticdroid.com"
+server="ftp.publicstaticdroid.com"
+port="21"
+
+# Change release type if --release is provided.
 releaseType="test"
 if [[ ($# -gt 2) ]]; then
 	releaseType="release"
 fi
+
+# Indicates the release milestone and the version number.
 release="featurecomplete"
 versionNum=$1
-currentDir=$PWD
+
+# Indicates what directory we are storing local release info in.
 toDir="${releaseType}/${release}_${versionNum}"
 
+# Makes the directories we need to place the files.
 `mkdir -p $toDir`
 `mkdir "${toDir}/include"`
 `mkdir "${toDir}/images"`
 
+# Build Java files into a .jar file and place in the release directory.
 workingDir="src/main/java/CloudComposerGroup/CloudComposer"
 tempDir="CloudComposerGroup/CloudComposer"
 `javac "${workingDir}/"*.java`
@@ -28,19 +44,19 @@ password=$2
 `rm -r $tempDir`
 `mv MidiPlayer.jar $toDir`
 
+# Change working directory and copy over the web application files.
 workingDir="src/main/webapp"
 `cp "${workingDir}/"*.html "${toDir}/"`
 `cp "${workingDir}/"*.php "${toDir}/"`
 `cp "${workingDir}/"*.js "${toDir}/include/"`
 `cp "${workingDir}/"*.css "${toDir}/include/"`
 
+# Change the working directory to our resources and copy over the images we use.
 workingDir="src/main/resources"
 `cp "${workingDir}/"* "${toDir}/images/"`
 
-username="cloudcomposer@publicstaticdroid.com"
-server="ftp.publicstaticdroid.com"
-port="21"
-
+# Begin an FTP session and proceed to copy all the files over.
+# Will make directories if they do not yet exist.
 ftp -ni $server <<END_SCRIPT
 quote USER $username
 quote PASS $password
